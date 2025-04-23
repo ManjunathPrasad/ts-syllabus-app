1078478fab9d7b08ffcfa5821d3a5e4bed34baa2
Manjunath <manjunathprasad99@gmail.com>
2025-04-23 22:02:58 +0530

    Added interface variants

---
+++ 7_interface_variations.ts
@@ -0,0 +1,40 @@
+//Basic Interface
+interface Person{
+    name: string;
+    age: number;
+}
+
+//optional properties
+interface Car{
+    brand: string;
+    model?: string; // optional 
+}
+
+// Readonly properties 
+interface Point{
+    readonly x: number;
+    readonly y: number;
+}
+
+// Function Types
+interface Greetings{
+    (name: string): string;
+}
+
+const greetUser1: Greetings = () => 'Hello ${name}';
+
+//Index signatures 
+interface Dictionary{
+    [key: string]: string;
+}
+
+// Interface extensions
+interface Animal {
+    name: string;
+}
+
+interface Dog extends Animal {
+    breed: string;
+}
+
+const d: Dog = {name: "Max", breed:"Beagle"};
