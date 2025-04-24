04700d27a96392d997231eb5604fd48a9e44947a
Manjunath <manjunathprasad99@gmail.com>
2025-04-25 01:16:14 +0530

    Updated tab name

--- Angular/angular-syllabus-app/src/app/app.component.ts
+++ Angular/angular-syllabus-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { UserProfileComponent } from './user-profile/user-profile.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-syllabus-app';
